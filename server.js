8a63960f48637cb693aa68796c6e4b8b7c09bd9b
Jason carrazco <carrazcojason10@gmail.com>
2021-06-10 23:45:19 -0500

    comment

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// const exerciseRoutes = require("./routes/exercises");
+// const workoutRoutes = require("./routes/workouts");
+const apiRoutes = require("./routes/api");
+const viewRoutes = require("./routes/views");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.use(apiRoutes);
+app.use(viewRoutes);
+// app.use(workoutRoutes);
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => console.log("listening on port: ", PORT));
